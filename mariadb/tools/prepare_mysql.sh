b7db24b29f1d08b35eace007291e645dda11693a
saxsori <saxxxsori@gmail.com>
2023-05-10 06:17:28 +0400

    fix when re run and re build them to not crash

--- mariadb/tools/prepare_mysql.sh
+++ mariadb/tools/prepare_mysql.sh
@@ -6,18 +6,23 @@ sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d
 
 sed -i 's/^#port/port/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+
+if [ ! -d /var/lib/mysql/mysql ]; then
 cp -r /mdlib/* /var/lib/mysql/
+fi
 
 service mysql start
 
 # find /
 
-# if ! mysql -u root -e 'use wpdb' ; then
+if [ ! -d /var/lib/mysql/wpdb ]; then
 	mysql -e "CREATE DATABASE wpdb;"
 	mysql -e "CREATE USER 'abrar'@'%' identified by 'abrar123';"
 	mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'abrar'@'%';"
 	mysql -e "FLUSH PRIVILEGES;"
-# fi
+	else
+	echo "db does exists ..."
+fi
 
 # service mysql stop
 
--- wordpress/tools/wp_files.sh
+++ wordpress/tools/wp_files.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+
+if [ ! -e /var/www/html/wp-config.php ]; then
+
 cp -r /wordpress/* /var/www/html/
+
 mv /wp-config.php /var/www/html/
-rm -rf /wordpress
+
+wp core install --allow-root --path=/var/www/html/ --url=localhost:442 --title="YOOO IT'S A B R A R" --admin_user=abrar --admin_password=abrar123 --admin_email=saxxxsori@gmail.com
+
+wp user create --allow-root  --path=/var/www/html/ sasori sasori@gmail.com --role=contributor --user_pass=password
+
+else
+	echo "wp does exist ..."
+fi
+
 chown -R www-data:www-data /var/www/html/
+
 sed -i 's/listen = .*/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-wp core install  --allow-root --path=/var/www/html/ --url=localhost:442 --title="YOOO IT IS A B R A R" --admin_user=abrar --admin_password=abrar123 --admin_email=saxxxsori@gmail.com
+
+rm -rf /wordpress
+
 php-fpm7.3 -F
