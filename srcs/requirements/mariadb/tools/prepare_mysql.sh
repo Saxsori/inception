134e13c59076bb5d82d86e19f317e4f3fea4b89a
saxsori <saxxxsori@gmail.com>
2023-05-12 21:46:40 +0400

    add some notes incase if I didn't submit anytine soon

--- srcs/requirements/mariadb/tools/prepare_mysql.sh
+++ srcs/requirements/mariadb/tools/prepare_mysql.sh
@@ -2,14 +2,17 @@
 
 # sed -i '/socket/s/^/#/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# to make it listen on all interfaces
 sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# ro make it use port instead of socket
 sed -i 's/^#port/port/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-
+# just for protection
 if [ ! -d /var/lib/mysql/mysql ]; then
 cp -r /mdlib/* /var/lib/mysql/
 fi
+
 
 service mysql start
 
@@ -24,6 +27,7 @@ if [ ! -d /var/lib/mysql/wpdb ]; then
 	echo "db does exists ..."
 fi
 
+# starting mysql in this way doesn't make the container stays running 
 service mysql stop
 
 # mysqld --console
--- srcs/requirements/wordpress/tools/wp_files.sh
+++ srcs/requirements/wordpress/tools/wp_files.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# to avoid doing this every time
 if [ ! -e /var/www/html/wp-config.php ]; then
 
 cp -r /wordpress/* /var/www/html/
 
 mv /wp-config.php /var/www/html/
 
+# required to install wp and not to show the installation page
 wp core install --allow-root --path=/var/www/html/ --url=localhost:442 --title="YOOO IT'S A B R A R" --admin_user=abrar --admin_password=abrar123 --admin_email=saxxxsori@gmail.com
 
+# create a user with role of contributor
 wp user create --allow-root --path=/var/www/html/ sasori sasori@gmail.com --role=contributor --user_pass=password
 
 echo "wp installed ..."
 
+# set up redis cache
 wp config --allow-root --path=/var/www/html/ set WP_CACHE_KEY_SALT 'sasori_chuck_isakey'
 
 wp config --allow-root --path=/var/www/html/ set WP_CACHE true
@@ -30,6 +34,7 @@ else
 	echo "wp does exist ..."
 fi
 
+# to avoid permission issues with nginx and php-fpm
 chown -R www-data:www-data /var/www/html/
 
 sed -i 's/listen = .*/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
